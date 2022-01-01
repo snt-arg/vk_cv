d4b0e5a798b0c228fe01b6bd94b860d612720a09
Paul <krepa098@gmail.com>
2022-01-01 19:10:28 +0100

    upload script

---
+++ upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PI_IP=raspberrypi.local
+PI_USER=pi
+PI_PW=raspberry
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# source -> destination
+#sshpass -p ${PI_PW} rsync -a $(cwd) ${PI_USER}@${PI_IP}:/home/${PI_USER}/vk_cn
+rsync --exclude 'target' --exclude '.git' -avz -e ssh ${SCRIPT_DIR} ${PI_USER}@${PI_IP}:/home/${PI_USER}/
+#sshpass -p '${PI_PW}' rsync --exclude 'target' --exclude '.git' -avz -e ssh ${SCRIPT_DIR} ${PI_USER}@${PI_IP}:/home/${PI_USER}/
