81377f182403508cc16d9823cad858ee42157ce6
Paul <krepa098@gmail.com>
2022-01-14 13:58:17 +0100

    fix pooling 4x shader

--- src/shaders/mean_pooling_4.comp.glsl
+++ src/shaders/mean_pooling_4.comp.glsl
@@ -6,6 +6,11 @@ layout(set = 0, binding = 1, rgba32f) uniform image2D resultImage;
 
 layout(constant_id = 2) const float inv_size = 1.0;
 
+const vec2 dp0 = vec2(0.5, 0.5);
+const vec2 dp1 = vec2(2.5, 0.5);
+const vec2 dp2 = vec2(0.5, 2.5);
+const vec2 dp3 = vec2(2.5, 2.5);
+
 void main() {
   ivec2 id = ivec2(gl_GlobalInvocationID.xy);
 
@@ -14,17 +19,12 @@ void main() {
   // texels which we then average.
   //
   // [0,0]---[1,0]---[2,0]---[3,0]
-  //   |   x   |       |   x   |      sample locations
+  //   |   x   |       |   x   |      sample locations: (0.5;0.5), (2.5;0.5)
   // [0,1]---[1,1]---[2,1]---[3,1]
   //   |       |       |       |
   // [0,2]---[1,3]---[2,3]---[3,3]
-  //   |   x   |       |   x   |      sample locations
+  //   |   x   |       |   x   |      sample locations: (0.5;2.5), (2.5;2.5)
   // [0,4]---[1,4]---[2,4]---[3,4]
-
-  const vec2 dp0 = vec2(0.5, 0.5);
-  const vec2 dp1 = vec2(1.5, 0.5);
-  const vec2 dp2 = vec2(0.5, 1.5);
-  const vec2 dp3 = vec2(1.5, 1.5);
 
   // scale down by a factor of 4
   vec4 d = texture(inputImageSampler, (id + dp0) * inv_size);
